bb054dc3e32a416882f59725e0dc6516c2755220
Nick <111998485+NicholasEng26@users.noreply.github.com>
2023-09-25 12:34:40 -0500

    Added config files

---
+++ public_html/News/home.php
@@ -0,0 +1,12 @@
+<?php
+    include 'includes/header.php';
+?>
+
+<section id="politics">
+    <h3>News Article One</h3>
+    <p>This is a news article description!</p>
+</section>
+
+<?php
+    include 'includes/footer.php';
+?>
---
+++ public_html/News/login.php
@@ -0,0 +1,12 @@
+<?php
+    include 'includes/header.php';
+?>
+
+<section id="login">
+    <h3>Please login here</h3>
+    <p>Registration over there!</p>
+</section>
+
+<?php
+    include 'includes/footer.php';
+?>
