cc6587665aa43389129ba34c43f9ec0a747d8c4e
Nick <111998485+NicholasEng26@users.noreply.github.com>
2023-09-25 11:58:29 -0500

    Added some code

---
+++ public_html/News/index.php
@@ -0,0 +1,19 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Your News Website</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+       HELLO <!-- Your website header content -->
+    </header>
+
+<!-- footer.php -->
+    <footer>
+        <!-- Your website footer content -->
+    </footer>
+</body>
+</html>
