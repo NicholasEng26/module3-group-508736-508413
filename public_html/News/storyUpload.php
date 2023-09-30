48fe4eb8cb8c5d71357b6157e88526418af639f4
SmallChungus1 <hanson.l@wustl.edu>
2023-09-30 03:30:40 -0600

    working story insertion into database

--- public_html/News/storyUpload.php
+++ public_html/News/storyUpload.php
@@ -18,10 +18,12 @@ include 'includes/header.php';
     <input type = "text" id = "storyTitle" name="storyTitleInput"/><br>
     <label for = "shortDesc">Short Description of Story:</label><br>
     <input type = "text" id = "shortDesc" name="shortDescInput"/><br>
-    <input type="radio" id="Politics" name="Politics" value="Politics">
+    <input type="radio" id="Politics" name="Category" value="Politics">
     <label for="Politics">Politics</label><br>
-    <input type="radio" id="Politics" name="Politics" value="Politics">
-    <label for="Politics">Politics</label><br>
+    <input type="radio" id="Business" name="Category" value="Business">
+    <label for="Business">Business</label><br>
+    <input type="radio" id="Technology" name="Category" value="Technology">
+    <label for="Technology">Technology</label><br>
     <label for = "storyContent">Content of Story:</label><br>
     <textarea id = "storyContent" name="storyContentInput"> 
 </textarea><br>
@@ -42,12 +44,33 @@ $title = $_POST['storyTitleInput'];
 $summary = $_POST['shortDescInput'];
 $content = $_POST['storyContentInput'];
 $media = $_POST['mediaLinkInput'];
+$cat = $_POST['Category'];
+$username = $_SESSION['currUser'];
+$articleid = null; //will use autoincrement
 
-echo "<h3>Story Preview: </h3><br>";
+
+echo "<h3>Story Preview for $username: </h3><br>";
+echo "<br> Category: $cat <br>";
 echo "<b>Titile:</b> <br>$title<br>";
 echo "<b>Short Description:</b> <br>$summary<br>";
 echo "<b>Content:</b> <br>$content<br>";
 echo "<iframe src = $media height=\"400vw\" width=\"400vw\" title=\"PLACEHOLDER\"><br>";
 
+$stmt = $mysqli->prepare("insert into articles (title, short_desc, category, content, image, article_id, owner) values (?, ?, ?, ?, ?, ?, ?)");
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}else{
+    echo"<br>connected ig?";
+}
+
+$stmt->bind_param('sssssis', $title, $summary, $cat, $content, $media, $articleid, $username);
+if(!$stmt->execute())
+{echo"<br>There was an error...<br>";
+}else{
+    echo "<br>something works<br>";
+}
+
+$stmt->close();
 
 ?>
