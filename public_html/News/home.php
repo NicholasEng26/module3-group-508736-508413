47d37ec10950767a23319f05266b02bb63e6a001
SmallChungus1 <hanson.l@wustl.edu>
2023-10-02 06:56:15 -0600

    home can display news stories completely iteratively now

--- public_html/News/home.php
+++ public_html/News/home.php
@@ -26,12 +26,12 @@
         $temp = array( $row["title"], $row["image"],  $row["short_desc"], $row["article_id"] );
         $politicsArticles[$index] = $temp;
         $index++;
-        printf("\t<li>%s %s %s</li>\n",
-            htmlspecialchars( $row["title"] ),
-            htmlspecialchars( $row["image"] ),
-            htmlspecialchars( $row["short_desc"] ),
-            htmlspecialchars( $row["article_id"] )        
-        );
+        // printf("\t<li>%s %s %s</li>\n",
+        //     htmlspecialchars( $row["title"] ),
+        //     htmlspecialchars( $row["image"] ),
+        //     htmlspecialchars( $row["short_desc"] ),
+        //     htmlspecialchars( $row["article_id"] )        
+        // );
 
     }
     echo "</ul>\n";
@@ -58,12 +58,12 @@
         $temp = array( $row["title"], $row["image"],  $row["short_desc"], $row["article_id"] );
         $businessArticles[$index] = $temp;
         $index++;
-        printf("\t<li>%s %s %s</li>\n",
-            htmlspecialchars( $row["title"] ),
-            htmlspecialchars( $row["image"] ),
-            htmlspecialchars( $row["short_desc"] ),
-            htmlspecialchars( $row["article_id"] )        
-        );
+        // printf("\t<li>%s %s %s</li>\n",
+        //     htmlspecialchars( $row["title"] ),
+        //     htmlspecialchars( $row["image"] ),
+        //     htmlspecialchars( $row["short_desc"] ),
+        //     htmlspecialchars( $row["article_id"] )        
+        // );
 
     }
     echo "</ul>\n";
@@ -90,12 +90,12 @@
         $temp = array( $row["title"], $row["image"],  $row["short_desc"], $row["article_id"] );
         $technologyArticles[$index] = $temp;
         $index++;
-        printf("\t<li>%s %s %s</li>\n",
-            htmlspecialchars( $row["title"] ),
-            htmlspecialchars( $row["image"] ),
-            htmlspecialchars( $row["short_desc"] ),
-            htmlspecialchars( $row["article_id"] )        
-        );
+        // printf("\t<li>%s %s %s</li>\n",
+        //     htmlspecialchars( $row["title"] ),
+        //     htmlspecialchars( $row["image"] ),
+        //     htmlspecialchars( $row["short_desc"] ),
+        //     htmlspecialchars( $row["article_id"] )        
+        // );
 
     }
     echo "</ul>\n";
@@ -153,26 +153,16 @@
     <section id="category">
         <h2 id="category_name"><u>Technology</u></h2>
         <div class="home-flex-container">
-            <section id="category-entry">
-                <h3>News #1</h3>
-                <img src="https://awlights.com/wp-content/uploads/sites/31/2017/05/placeholder-news.jpg" height="200vw" width="350vw" title="PLACEHOLDEr">
-                <p>This is a news article description!</p>
-            </section>
-            <section id="category-entry">
-                <h3>News #2</h3>
-                <img src="https://awlights.com/wp-content/uploads/sites/31/2017/05/placeholder-news.jpg" height="200vw" width="350vw" title="PLACEHOLDEr">
-                <p>This is a news article description!</p>
-            </section>
-            <section id="category-entry">
-                <h3>News #3</h3>
-                <img src="https://awlights.com/wp-content/uploads/sites/31/2017/05/placeholder-news.jpg" height="200vw" width="350vw" title="PLACEHOLDEr">
-                <p>This is a news article description!</p>
-            </section>
-            <section id="category-entry">
-                <h3>News #4</h3>
-                <img src="https://awlights.com/wp-content/uploads/sites/31/2017/05/placeholder-news.jpg" height="200vw" width="350vw" title="PLACEHOLDEr">
-                <p>This is a news article description!</p>
-            </section>
+        <?php for ($i = 0; $i < count($technologyArticles); $i++) { ?>
+                <section id="category-entry">
+                    <h3><?php echo($technologyArticles[$i][0]); ?></h3>
+                    <form action="article.php" method="get">
+                        <input hidden type="text" id="article_id" name="article_id" value= <?php echo($technologyArticles[$i][3]); ?>>
+                        <input type="image" src=<?php echo($technologyArticles[$i][1]); ?> height="200vw" title="PLACEHOLDER" width="350vw" alt="Submit">
+                    </form>
+                    <p><?php echo($technologyArticles[$i][2]); ?></p>
+                </section>
+            <?php } ?>
         </div>
     </section>
 </div>
